4a4e51b70a18fdf8d3acb9e11018bddb7bbc3531
Patrick <patrickpierre2015@gmail.com>
2024-09-02 01:24:28 -0300

    refeito o projeto de inversão de string

--- inversor/index.php
+++ inversor/index.php
@@ -1,6 +1,13 @@
 <?php
 function inverterString($str) {
-    return strrev($str);
+    $inverted = '';
+    $length = strlen($str);
+
+    for ($i = $length - 1; $i >= 0; $i--) {
+        $inverted .= $str[$i];
+    }
+
+    return $inverted;
 }
 
 $string = "Target Sistemas";
